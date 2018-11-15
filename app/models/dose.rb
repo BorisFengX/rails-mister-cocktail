9600dba266b37f412ae46a0e8625ca18373b597d
BorisFeng <boris.feng@decathlon.com>
2018-11-14 20:59:09 -0800

    3 models done

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,7 @@
 class Dose < ApplicationRecord
+  validates :description, presence: true
+  validates :ingredient_id, :uniqueness => {:scope => :cocktail_id }
+  # validates :cocktail_id, :ingredient_id, uniqueness: true
   belongs_to :cocktail
   belongs_to :ingredient
 end

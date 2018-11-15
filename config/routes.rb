c30130dc677c17a69223b403a911ba1887632f7f
kai wang <wangkai199385@126.com>
2018-11-15 13:57:45 +0800

    done routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'cocktails', to: 'cocktails#index', as: :cocktails
+  # get 'cocktail/:id', to: 'cocktails#show', as: :cocktail
+
+  # get 'cocktails/new', to: 'cocktails#new', as: :new_cocktail
+  # post 'cocktails', to: 'cocktalis#create'
+
+  # get 'cocktails/:cocktail_id'
+
+  resources :cocktails, except: [:edit, :update, :delete] do
+    resources :doses, only: [:new, :create, :delete]
+  end
+
+  root to: 'cocktails#index'
 end
